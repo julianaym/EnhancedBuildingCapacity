0a41b972515bfaf8a5d8b632727b92ec711abe7b
julianaym <julianaym@users.noreply.github.com>
2015-05-02 00:36:42 +0200

    removed some debugging code

--- EnhancedBuildingCapacity/LoadingExtension.cs
+++ EnhancedBuildingCapacity/LoadingExtension.cs
@@ -13,14 +13,6 @@ namespace EnhancedBuildingCapacity
 
                 Debug.PrintMessage("Successfully loaded mod v" + Debug.GetVersion());
 
-                Debug.PrintMessage(XmlConfig.config.Residential_LowDensity[Configuration.Building.Levels.Level1].Capacity);
-                Debug.PrintMessage(XmlConfig.config.Residential_LowDensity[Configuration.Building.Levels.Level1].ElectricityConsumption);
-                Debug.PrintMessage(XmlConfig.config.Residential_LowDensity[Configuration.Building.Levels.Level1].GarbageAccumulation);
-                Debug.PrintMessage(XmlConfig.config.Residential_LowDensity[Configuration.Building.Levels.Level1].IncomeAccumulation);
-                Debug.PrintMessage(XmlConfig.config.Residential_LowDensity[Configuration.Building.Levels.Level1].SewageAccumulation);
-                Debug.PrintMessage(XmlConfig.config.Residential_LowDensity[Configuration.Building.Levels.Level1].WaterConsumption);
-
-
                 Patch.PatchEveryBuildingAI();
             }
         }
